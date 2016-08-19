5c253938df757ee7cbcabc708da9b89e9ba26725
Sami Aibeche <samiaibeche@gmail.com>
2016-08-19 13:58:36 +0200

    Création de l'entité Tags

---
+++ WebAwards/src/WebAwardsBundle/Entity/Tags.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace WebAwardsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tags
+ *
+ * @ORM\Table(name="tags")
+ * @ORM\Entity(repositoryClass="WebAwardsBundle\Repository\TagsRepository")
+ */
+class Tags
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="tag_name", type="string", length=125)
+     */
+    private $tagName;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set tagName
+     *
+     * @param string $tagName
+     *
+     * @return Tags
+     */
+    public function setTagName($tagName)
+    {
+        $this->tagName = $tagName;
+
+        return $this;
+    }
+
+    /**
+     * Get tagName
+     *
+     * @return string
+     */
+    public function getTagName()
+    {
+        return $this->tagName;
+    }
+}
+
