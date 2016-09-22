7614f608178632b1f4708e45a719d834a30b069a
Sami Aibeche <samiaibeche@gmail.com>
2016-09-22 15:14:45 +0200

    Test mois - année

--- WebAwards/src/WebAwardsBundle/Util/WebAwards.php
+++ WebAwards/src/WebAwardsBundle/Util/WebAwards.php
@@ -48,4 +48,70 @@ class WebAwards {
         //Champs OK
         return true;
     }
+
+    public static function checkLastMonthWinner($arrData){
+
+        //Champs vide
+        if(empty($arrData)){
+            return "empty";
+        }
+        //Champs invalide, Array attendu
+        if(!(is_array($arrData))){
+             return "invalidArray";
+        }
+        //Taille du tableau invalid
+        if(count($arrData) !== 2){
+            return "invalid";
+        }
+        //Clé inexistante
+        if(!(array_key_exists('month', $arrData))){
+            return "invalidMonthKey";
+        }
+        //Seconde clé inexistante
+        if(!(array_key_exists('year', $arrData))){
+            return "invalidYearKey";
+        }
+
+        $currMonth = $arrData["month"];
+        $currYear = $arrData["year"];
+
+        //Données non numérique
+        if(!(is_numeric($currMonth)) || !(is_numeric($currYear))){
+            return "invalidData";
+        }
+
+        $currMonth = (int) $arrData["month"];
+        $currYear  = (int) $arrData["year"];
+
+        //Mois non compris entre 1 et 12
+        if( ($currMonth>12) || ($currMonth<1) ){
+            return "invalidMonth";
+        }
+
+        $month = (int) date("m");
+        $year = (int) date("Y");
+
+        //Mois de jancier de cette année
+        if($currMonth == 1 && $currYear == $year){
+            return "dateOk";
+        }
+        //Mois de décembre de l'année passée
+        if($currMonth == 1 && $currYear == ($year - 1)){
+            return "dateLastOk";
+        }
+
+        //Année passée
+        if( ($currYear<2016) ) {
+            return "invalidYear";
+        }
+        //Mois je janvier
+        if( $currMonth == 1){
+            return "janvier";
+        }
+        //Données valide
+        if( ($currMonth <= 12) || ($currMonth >=1 ) && $currYear == $year){
+            return true;
+        }
+        
+    }
 }
--- WebAwards/tests/WebAwardsBundle/Util/WebAwardsTest.php
+++ WebAwards/tests/WebAwardsBundle/Util/WebAwardsTest.php
@@ -20,7 +20,32 @@ class WebAwardsTest extends \PHPUnit_Framework_TestCase {
             $this->assertEquals(true, WebAwards::checkVote(8));
             $this->assertEquals(true, WebAwards::checkVote(8.5));
             $this->assertEquals(true, WebAwards::checkVote(8.55555221100));
-        
+
+
+
+            $this->assertEquals("empty", WebAwards::checkLastMonthWinner([]));
+            $this->assertEquals("invalidArray", WebAwards::checkLastMonthWinner("qsd"));
+            $this->assertEquals("invalidArray", WebAwards::checkLastMonthWinner(1233));
+            $this->assertEquals("invalid", WebAwards::checkLastMonthWinner(["1"]));
+            $this->assertEquals("invalid", WebAwards::checkLastMonthWinner(["1","2","3"]));
+            $this->assertEquals("invalidMonthKey", WebAwards::checkLastMonthWinner(["1", "2"]));
+            $this->assertEquals("invalidMonthKey", WebAwards::checkLastMonthWinner(["dev"=>"1", "2"]));
+            $this->assertEquals("invalidYearKey", WebAwards::checkLastMonthWinner(["month"=>"1", "2"]));
+            $this->assertEquals("invalidYearKey", WebAwards::checkLastMonthWinner(["month"=>"1", "dev"=>"2"]));
+            $this->assertEquals("invalidData", WebAwards::checkLastMonthWinner(["month" => "dev", "year" => "test"]));
+            $this->assertEquals("invalidMonth", WebAwards::checkLastMonthWinner(["month" => "0", "year" => "11"]));
+            $this->assertEquals("invalidMonth", WebAwards::checkLastMonthWinner(["month" => "13", "year" => "11"]));
+            $this->assertEquals("invalidYear", WebAwards::checkLastMonthWinner(["month" => "12", "year" => "2015"]));
+            $this->assertEquals("invalidYear", WebAwards::checkLastMonthWinner(["month" => "12", "year" => "-2015"]));
+            $this->assertEquals("janvier", WebAwards::checkLastMonthWinner(["month" => "1", "year" => "2017"]));
+            $this->assertEquals("dateOk", WebAwards::checkLastMonthWinner(["month" => "1", "year" => "2016"]));
+            $this->assertEquals("dateLastOk", WebAwards::checkLastMonthWinner(["month" => "1", "year" => "2015"]));
+            $this->assertEquals(true, WebAwards::checkLastMonthWinner(["month" => "8", "year" => "2016"]));
+            $this->assertEquals(true, WebAwards::checkLastMonthWinner(["month" => "12", "year" => "2016"]));
+            $this->assertEquals(true, WebAwards::checkLastMonthWinner(["month" => "2", "year" => "2016"]));
+
+
+
         }
 
     }
